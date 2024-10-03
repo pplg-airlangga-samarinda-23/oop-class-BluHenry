28e54b7dfd97e2515bbaafedd04c47069b7c7770
tes <geogeryanemory@gmail.com>
2024-10-03 12:06:39 +0800

    selesai 2 soal tugas (SOAL 3 RILL TIME CUUYH

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,9 +2,55 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
+    private $panjang;
+    private $lebar;
+    private $tinggi;
 
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang, $lebar, $tinggi)
+    {
+        $this -> panjang = $panjang;
+        $this -> lebar = $lebar;
+        $this -> tinggi = $tinggi;
+    }
+
+    public function setpanjang($panjang) 
+    {
+        $this -> panjang = $panjang;
+    }
+
+    public function setlebar($lebar) 
+    {
+        $this -> lebar = $lebar;
+    }
+
+    public function settinggi($tinggi) 
+    {
+        $this -> tinggi = $tinggi;
+    }
+
+    public function getpanjang(){
+        return $this -> panjang;
+    }
+
+    public function getlebar(){
+        return $this -> lebar;
+    }
+
+    public function gettinggi(){
+        return $this -> tinggi;
+    }
+
+    public function HitungVolume()
+    {
+        echo ($this -> panjang * $this -> lebar) * $this -> tinggi  ;
+    }
+
+    public function hitungLuasPermukaan()
+    {
+        echo 2 * (($this -> panjang * $this -> lebar) + ($this -> panjang * $this -> tinggi) + ($this -> lebar * $this -> tinggi)) ;    
+    }
 
 
     // definisikan metode menghitung volume, luas permukaan, 
@@ -12,6 +58,23 @@ class Balok
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
+echo "Balok 1 <br>";
+echo "volume : ";
+echo $balok_1 -> HitungVolume();
+echo "<br>" ;
+echo "Luar permukaan :  ";
+echo $balok_1 -> hitungLuasPermukaan();
+echo "<br>";
+echo "Balok 2 <br>";
+echo "volume : ";
+echo $balok_2 -> HitungVolume();
+echo "<br>" ;
+echo "Luar permukaan :  ";
+echo $balok_2 -> hitungLuasPermukaan();
+
+
+
+
 
 // contoh output: Balok
 
@@ -22,3 +85,5 @@ $balok_2 = new Balok(3, 10, 12);
 // Balok 2
 // Volume: 360
 // Luas Permukaan: 372
+
+?>
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -3,22 +3,129 @@
 class Siswa
 {
     // deklarasikan atribut-atribut kelas Siswa
+    private $nama ;
+    private $nisn ;
+    private $alamat ;
+    private $jurusan ;
+    private $tanggal_Lahir;
+    
 
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($nama, $nisn, $alamat, $jurusan, $tanggalLahir) {
+        $this->nama = $nama;
+        $this->nisn = $nisn;
+        $this->alamat = $alamat;
+        $this->jurusan = $jurusan;
+        $this->tanggal_Lahir = $tanggalLahir; // Format tanggal: YYYY-MM-DD
+    }
+
+    public function setNama($nama) {
+        $this->nama = $nama;
+    }
+
+    public function setNisn($nisn) {
+        $this->nisn = $nisn;
+    }
+
+    public function setAlamat($alamat) {
+        $this->alamat = $alamat;
+    }
+
+    public function setJurusan($jurusan) {
+        $this->jurusan = $jurusan;
+    }
+
+    public function setTanggalLahir($tanggal_Lahir) {
+        $this->tanggal_Lahir = $tanggal_Lahir;
+    }
+
+    // Getter untuk mendapatkan nilai atribut
+    public function getNama() {
+        return $this->nama;
+        echo $this -> nama;
+    }
+
+    public function getNisn() {
+        return $this->nisn;
+        echo $this -> nisn;
+    }
+
+    public function getAlamat() {
+        return $this->alamat;
+        echo $this -> alamat;
+    }
+
+    public function getJurusan() {
+        return $this->jurusan;
+        echo $this -> jurusan;
+    }
+
+    public function getTanggalLahir() {
+        return $this->tanggal_Lahir;
+        echo $this -> tanggal_Lahir;
+    }
 
 
     // buat metode untuk menghitung umur siswa
+
     public function hitungUmur() {
-
-
-        return $umur;
+        $tanggal_Lahir = new DateTime($this -> tanggal_Lahir);
+        $tanggalSekarang = new DateTime(); //tanggal saat ini
+        $umur = $tanggal_Lahir -> diff($tanggalSekarang);
+        return $umur->y; //mengembalikan umur dalam tahun 
+        
     }
 }
 
 // buat dua buah objek siswa
 $siswa_1 = new Siswa("Bambang", "1234", "Loa Bakung", "PPLG", "27-11-2006");
-$siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2007");
+$siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2006");
+
+echo "nama siswa 1 = " . $siswa_1 -> getNama();
+echo "<br>";
+echo "Nisn siswa 1 = " . $siswa_1 -> getNisn();
+echo "<br>";
+echo "alamat tempat siswa 1 = " . $siswa_1 -> getAlamat();
+echo "<br>";
+echo "Jurusan yg dipilih siswa 1 = " . $siswa_1 -> getJurusan();
+echo "<br>";
+echo "tanggal lahir siswa 1 = " . $siswa_1 -> getTanggalLahir();
+echo "<br>";
+echo "nama siswa 2 = " . $siswa_2-> getNama();
+echo "<br>";
+echo "Nisn siswa 2 = " . $siswa_2 -> getNisn();
+echo "<br>";
+echo "alamat? tempat siswa 2 = " . $siswa_2 -> getAlamat();
+echo "<br>";
+echo "Jurusan yg dipilih siswa 2 = " . $siswa_1 -> getJurusan();
+echo "<br>";
+echo "tanggal lahir siswa 2 = " . $siswa_1 -> getTanggalLahir();
+echo "<br>";
+
+
+
+
+$umursiswa1 = $siswa_1->hitungUmur();
+$umursiswa2 = $siswa_2->hitungUmur();
+echo "<br>";
+echo "<br>";
+echo "siswa 1 berumur = " . $umursiswa1;  
+echo "<br>";
+echo "siswa 2 berumur = " . $umursiswa2;
+echo "<br>";
+echo "<br>";
+if ($umursiswa1 > $umursiswa2){
+    echo "siswa atas nama : " . $siswa_1 -> getNama() . " <br> lebih tua dari siswa beratas namakan : " . $siswa_2 -> getNama();
+} elseif ($umursiswa1 < $umursiswa2){
+    echo "siswa atas nama : " . $siswa_1 -> getNama() . "<br> lebih muda dari siswa beratas namakan : " . $siswa_2 -> getNama();
+} else {
+    echo "siswa : " . $siswa_1 -> getNama() . " dan siswa : " . $siswa_2 -> getNama() . "<br> memiliki umur yg sama";
+}
+
+
+
+
 
 // contoh output:
 // Bambang lebih tua dibandingkan Bahrun.
